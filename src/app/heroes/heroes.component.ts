cc94f01ffcff1b892bb044d4b78391a346839a60
Zeroeroseix <mulomedicus@hotmail.com>
2023-05-11 20:46:19 +0200

    4. Add Service (Síncrono)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.interface';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  heroes = HEROES
+  heroes!: Hero[]
   hero: Hero = {
     id: 1,
     name: "Windstorm"
   };
   selectedHero!: Hero
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
