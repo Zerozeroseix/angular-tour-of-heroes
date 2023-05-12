a38ac0e030c8a7a7f489c7e469f3f4b74c45416d
Zeroeroseix <mulomedicus@hotmail.com>
2023-05-12 19:24:13 +0200

    6. GET DATA FROM SERVER (5) 1. Added Save button in HeroDetail 2. UpdateHero in HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,6 +15,11 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
@@ -35,6 +40,17 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+
 
 
   private getImage(heroName: string) {
